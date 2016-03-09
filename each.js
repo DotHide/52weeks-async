607f0967abce1f3202a7202007e28c89b327903a
Jack Zhang <dothide@gmail.com>
2016-03-09 10:41:44 +0800

    丰富 each.js 的示例方法，增加对每个访问的运行时间计算，并输出总的运行时间

--- each.js
+++ each.js
@@ -1,4 +1,5 @@
 var fetch = require('node-fetch');
+var present = require('present');
 var async = require('async');
 
 var urls = [
@@ -11,30 +12,66 @@ var urls = [
 
 // 访问网址（3秒超时）
 var fetchUrls = function(url, callback) {
+  var start = present();
   return fetch(url, {
     timeout: 3000
   }).then(function(res) {
-    console.log(url, res.statusText);
-    callback(null, url);
+    var end = present();
+    console.log('===> Reached: ', url, res.statusText, (end - start).toFixed(2) + 'ms');
+    callback();
   }).catch(function(err) {
     callback(err);
   });
 }
 
-// 并行执行访问网址操作，执行顺序不保证
-console.time('each');
-async.each(urls, fetchUrls, function(err) {
-  console.timeEnd('each');
-});
+var a = function(callback) {
+  var start = present();
+  // 并行执行访问网址操作，执行顺序不保证
+  async.each(urls, fetchUrls, function(err) {
+    var end = present();
+    if (err) { console.log('xxx> Error: ', err); }
+    console.log('===> each Finished.');
+    callback(null, 'each: ' + (end - start).toFixed(2) + 'ms');
+  });
+}
 
-// 串行执行访问网址操作，执行顺序依据数组元素顺序
-console.time('eachSeries');
-async.eachSeries(urls, fetchUrls, function(err) {
-  console.timeEnd('eachSeries');
-});
+var b = function(callback) {
+  var start = present();
+  // 串行执行访问网址操作，执行顺序依据数组元素顺序
+  async.eachSeries(urls, fetchUrls, function(err) {
+    var end = present();
+    if (err) { console.log('xxx> Error: ', err); }
+    console.log('===> eachSeries Finished.');
+    callback(null, 'eachSeries:' + (end - start).toFixed(2) + 'ms');
+  });
+}
 
-// 并行执行访问网址操作，但限制最多同时执行 2 个，同样不保证执行顺序
-console.time('eachLimit');
-async.eachLimit(urls, 3, fetchUrls, function(err) {
-  console.timeEnd('eachLimit');
-});
+var c = function(callback) {
+  var start = present();
+  // 并行执行访问网址操作，但限制最多同时执行 2 个，同样不保证执行顺序
+  console.time('eachLimit');
+  async.eachLimit(urls, 3, fetchUrls, function(err) {
+    var end = present();
+    if (err) { console.log('xxx> Error: ', err); }
+    console.log('===> eachLimit Finished.');
+    callback(null, 'eachLimit:' + (end - start).toFixed(2) + 'ms');
+  });
+}
+
+var d = function(callback) {
+  var start = present();
+  async.series([
+      a,
+      b,
+      c
+    ],
+    // optional callback
+    function(err, results) {
+      // results is now equal to ['one', 'two']
+      var end = present();
+      console.log('result: ', results);
+      console.log('Total: ', (end - start).toFixed(2) + 'ms');
+    });
+}
+
+d();
